ba35c5d084b96bd1f9918a2c2df3d2cc3a8733b7
Reakain <reakain@users.noreply.github.com>
2022-07-15 12:03:56 -0700

    added health change, lives, and death

--- GTMK22/Assets/Scripts/Player.cs
+++ GTMK22/Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public int health { get; private set; }
+    public int lives { get; private set; }
     public int money { get; private set; }
     public Die[] dice { get; private set; }
     // Start is called before the first frame update
@@ -17,5 +18,20 @@ public class Player : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public ChangeHealth(int changeValue)
+    {
+        health += changeValue;
+        if( health <= 0)
+        {
+            PlayerDeath();
+        }
+    }
+
+    public void PlayerDeath()
+    {
+        //TODO: DIE DIE DIE DIE
+        return;
     }
 }

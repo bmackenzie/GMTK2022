2f28abfda3d1617a87ca70a2fcf5b44cb67b45f9
Reakain <reakain@users.noreply.github.com>
2022-07-15 11:56:08 -0700

    added empty player script

---
+++ GTMK22/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int health { get; private set; }
+    public int money { get; private set; }
+    public Die[] dice { get; private set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

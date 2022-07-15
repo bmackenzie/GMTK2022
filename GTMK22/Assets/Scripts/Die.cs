ffb8b9412d4f6dec0377260c5994b3f2254df423
Ryan Quick <rquick14@gmail.com>
2022-07-15 14:18:31 -0700

    Initial functions for roll dice, change side, set side number

--- GTMK22/Assets/Scripts/Die.cs
+++ GTMK22/Assets/Scripts/Die.cs
@@ -4,33 +4,44 @@ using UnityEngine;
 
 public class Die : MonoBehaviour
 {
-    public int numSides = 6;
+    public int numSides { get; private set; }
     public int[] sideAction { get; private set; }
+
+    
     
     // Start is called before the first frame update
     void Start()
     {
-        // TODO: Generate side actions?
-        // TODO: Health?
+        SetNumSides(6);
+        sideAction = new int[numSides];
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        return;
+    }
+
+    public void SetNumSides(int sides)
+    {
+        numSides = sides;   
+        return;
     }
 
     public void ChangeSide(int side, int action)
     {
         // TODO: Update side and action
+        sideAction[side] = action;
         return;
     }
 
-    public int RollDie()
+    public int[] RollDie()
     {
         // TODO: Get a side action and return it?
-        return -1;
+        int currentFace = Random.Range(0, numSides-1);
+        int currentAction = sideAction[currentFace];
+        return new int[] {currentFace, currentAction};
     }
 
     void Merge(Die secondDie, Die thirdDie)

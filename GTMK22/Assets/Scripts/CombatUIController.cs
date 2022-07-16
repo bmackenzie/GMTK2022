27722afec6212995cfcd717587081828ec1fdd11
Ryan Quick <rquick14@gmail.com>
2022-07-15 19:01:11 -0700

    Added Lives icon function

--- GTMK22/Assets/Scripts/CombatUIController.cs
+++ GTMK22/Assets/Scripts/CombatUIController.cs
@@ -6,26 +6,43 @@ using UnityEngine.UI;
 public class CombatUIController : MonoBehaviour
 {
     public Image playerHealthBarImage;
-  public Image enemyHealthBarImage;
-
+    public Image enemyHealthBarImage;
+    public Image playerLivesIcon;
     public Player player;
     public Enemy enemy;
+    public GameObject gameobject;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<Player>();
+        enemy = FindObjectOfType<Enemy>();
+        //gameObject = FindObjectOfType<GameObject>();
+    }
+
+    public void CreateObject(string prefabName) 
+    {
+        for(int lvs=1;lvs <= player.lives; lvs++)
+        {
+            GameObject lifeIcon = GameObject.Instantiate(Resources.Load("LivesIcon")) as GameObject;
+            lifeIcon.transform.position += new Vector3(10*lvs, 0, 0);
+        }
+    
+    }
 
     public void UpdateHealthBar()
     {
         playerHealthBarImage.fillAmount = Mathf.Clamp(player.health / player.maxHealth, 0, 1f);
         enemyHealthBarImage.fillAmount = Mathf.Clamp(enemy.health / enemy.maxHealth, 0, 1f);
-
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateRoundNumber()
     {
-        
+        return;
     }
+
+    public void UpdateLivesIcons()
+    {
+        CreateObject("LiveIcon");
+    }
+
 }

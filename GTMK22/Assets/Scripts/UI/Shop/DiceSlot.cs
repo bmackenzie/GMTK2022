3d692618719d759a212596f4ef407493de2c1512
Reakain <reakain@users.noreply.github.com>
2022-07-16 16:57:45 -0700

    updated diceslot and shop die stuff to get data from player stuff, and on destruction update player stuff with their details

--- GTMK22/Assets/Scripts/UI/Shop/DiceSlot.cs
+++ GTMK22/Assets/Scripts/UI/Shop/DiceSlot.cs
@@ -5,11 +5,24 @@ using UnityEngine.EventSystems;
 
 public class DiceSlot : MonoBehaviour, IDropHandler
 {
+    public int numSides;
+    private Die slimeDetails;
+
+    void Awake()
+    {
+        slimeDetails = FindObjectOfType<Player>().GetDieDetails(numSides);
+
+    }
     public void OnDrop(PointerEventData eventData)
     {
         if(eventData.pointerDrag != null)
         {
             Destroy(eventData.pointerDrag);
         }
+    }
+
+    public void OnDestroy()
+    {
+        FindObjectOfType<Player>().UpdateDieDetails(numSides, slimeDetails);
     }
 }

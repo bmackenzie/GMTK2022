0782fd479fb3eb516e7c45045885fb8323f48ae3
Ryan Quick <rquick14@gmail.com>
2022-07-15 17:56:15 -0700

    Small push with organization cleanup

--- GTMK22/Assets/Scripts/CombatUIController.cs
+++ GTMK22/Assets/Scripts/CombatUIController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthBarController : MonoBehaviour
+public class CombatUIController : MonoBehaviour
 {
     public Image playerHealthBarImage;
   public Image enemyHealthBarImage;

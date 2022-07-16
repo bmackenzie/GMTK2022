849859ae7525e81355a3e11fe80ca98024790a66
Reakain <reakain@users.noreply.github.com>
2022-07-16 14:42:23 -0700

    updated scene transition stuff

--- GTMK22/Assets/Scripts/GameManager.cs
+++ GTMK22/Assets/Scripts/GameManager.cs
@@ -61,7 +61,7 @@ public class GameManager : MonoBehaviour
         if(victory)
         {
             rounds++;
-            this.dialogueRunner.StartDialogue("SecondDialog");
+            this.dialogueRunner.StartDialogue("BattleEnd");
             GoToShop();
         }
         else

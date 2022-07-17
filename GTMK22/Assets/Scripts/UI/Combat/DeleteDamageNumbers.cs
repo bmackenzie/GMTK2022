6f44e0efb25b32f6c16f394571648e2b1d8b48f0
Ryan Quick <rquick14@gmail.com>
2022-07-17 03:50:03 -0700

    Added prefab of damage special effect

---
+++ GTMK22/Assets/Scripts/UI/Combat/DeleteDamageNumbers.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteDamageNumbers : MonoBehaviour
+{
+    public void DestroyNumParent()
+    {
+        Destroy(this.gameObject);
+    }
+}
